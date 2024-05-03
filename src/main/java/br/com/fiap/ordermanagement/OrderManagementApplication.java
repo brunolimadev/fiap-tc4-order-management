09792a52480c6336647142e9feb95da6b652cf0f
brunolimadev <bruno.lima.dev@outlook.com>
2024-05-03 08:53:18 -0300

    feat: add projet files

---
+++ src/main/java/br/com/fiap/ordermanagement/OrderManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementApplication.class, args);
+	}
+
+}
